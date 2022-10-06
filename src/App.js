c9ca039282aa4115d94c8780650c73fd70c0b811
ShaikhZuber08 <90678325+ShaikhJuber@users.noreply.github.com>
2022-10-06 17:53:12 +0530

    remove console's in App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   const { data, isLoading, error } = useQuery("products", getProduct);
 
-  console.log(data);
+ 
 
   const getTotaleItems = (items) =>
     items.reduce((ack, item) => ack + item.amount, 0);
